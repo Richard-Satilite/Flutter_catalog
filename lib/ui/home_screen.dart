4d637e793ddea9fc1c060370e9c3f3041de66be9
Richard <richardsatilite.dev@gmail.com>
2025-01-17 21:11:15 -0300

    Flutter-catalog: Added assets folder with images and included in the home screen

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/ui/style/colors.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -7,7 +8,61 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text("HOME"),
+        child: Container(
+          padding: EdgeInsets.only(top: 64, right: 32, left: 32, bottom: 32),
+          child: Column(
+            children: <Widget>[
+              Stack(
+                alignment: Alignment.center,
+                children: [
+                  Text(
+                    "FLUTTER",
+                    style: TextStyle(
+                      fontFamily: 'Public Sans',
+                      fontWeight: FontWeight.bold,
+                      fontSize: 80,
+                      foreground: Paint()
+                        ..color = AppColor.homeText1Fore
+                        ..style = PaintingStyle.stroke
+                        ..strokeWidth = 8,
+                    ),
+                  ),
+                  Text(
+                    "FLUTTER",
+                    style: TextStyle(
+                      fontSize: 82,
+                      color: AppColor.homeText1,
+                    ),
+                  )
+                ],
+              ),
+              Text(
+                "CATALOG",
+                style: TextStyle(
+                    fontFamily: 'Public Sans',
+                    fontStyle: FontStyle.italic,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 60,
+                    color: AppColor.homeText2),
+              ),
+              Stack(
+                alignment: Alignment.center,
+                children: [
+                  Image.asset(
+                    "assets/images/frame_blob.png",
+                    height: 340,
+                  ),
+                  Image.asset(
+                    "assets/images/home.png",
+                    height: 272,
+                  ),
+                ],
+              ),
+              Text(
+                  "O lugar para ideal para busca, salva e organizar seus filmes favoritos!"),
+            ],
+          ),
+        ),
       ),
     );
   }
---
+++ lib/ui/style/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const Color homeText1 = Color(0xFFFFD740);
+  static const Color homeText1Fore = Color.fromARGB(143, 255, 214, 64);
+  static const Color homeText2 = Color(0xFFFFFFFF);
+}
