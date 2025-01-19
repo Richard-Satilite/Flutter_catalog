1082adeaf5acc30c6716b0dced8eb1a5a89c6017
Richard <richardsatilite.dev@gmail.com>
2025-01-19 13:37:28 -0300

    Flutter-Catalog: Modified search_movies screen list View style

--- lib/ui/components/movie_card.dart
+++ lib/ui/components/movie_card.dart
@@ -6,40 +6,43 @@ class MovieCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
-        Container(
-          width: 90,
-          height: 120,
-          decoration: BoxDecoration(
-            color: Color(0xFFFFFFFF),
-            borderRadius: BorderRadius.circular(8),
+    return Padding(
+      padding: const EdgeInsets.only(top: 32),
+      child: Row(
+        children: <Widget>[
+          Container(
+            width: 90,
+            height: 120,
+            decoration: BoxDecoration(
+              color: Color(0xFFFFFFFF),
+              borderRadius: BorderRadius.circular(8),
+            ),
+            margin: const EdgeInsets.only(right: 32),
           ),
-          margin: const EdgeInsets.only(right: 32),
-        ),
-        Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(bottom: 6),
-              child: Text(
-                "Título",
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.w600,
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(bottom: 6),
+                child: Text(
+                  "Título",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                  ),
                 ),
               ),
-            ),
-            Text(
-              "Lançamento: xxxx-xx-xx",
-              style: TextStyle(
-                color: AppColor.searchReleaseText,
+              Text(
+                "Lançamento: xxxx-xx-xx",
+                style: TextStyle(
+                  color: AppColor.searchReleaseText,
+                ),
               ),
-            ),
-          ],
-        )
-      ],
+            ],
+          )
+        ],
+      ),
     );
   }
 }
--- lib/ui/screens/search_movies.dart
+++ lib/ui/screens/search_movies.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/ui/components/movie_card.dart';
+import 'package:flutter_catalog/ui/style/colors.dart';
 
 class SearchMovies extends StatelessWidget {
   const SearchMovies({super.key});
@@ -7,7 +8,48 @@ class SearchMovies extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: MovieCard(),
+      child: CustomScrollView(
+        slivers: <Widget>[
+          SliverToBoxAdapter(
+            child: Image.asset(
+              "assets/images/movie.png",
+              height: 128,
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Text(
+              "Filmes Populares",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 26,
+                fontWeight: FontWeight.bold,
+                color: AppColor.searchReleaseText,
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.only(top: 32),
+              child: TextField(
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w500,
+                ),
+                decoration: InputDecoration(
+                    hintText: "Pesquisar",
+                    prefixIcon: Icon(Icons.search),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(25),
+                    )),
+              ),
+            ),
+          ),
+          SliverList.builder(
+            itemBuilder: (context, index) => MovieCard(),
+            itemCount: 10,
+          ),
+        ],
+      ),
     );
   }
 }
